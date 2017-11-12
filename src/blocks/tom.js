8ad6d04c05df37189efd8d92d29b8679028bcc6c
Antoine <com.acordier@gmail.com>
2017-11-12 15:44:17 +0100

    Refactor(kick): Fix gain value on start and adjust lowshelf values

--- src/blocks/tom.js
+++ src/blocks/tom.js
@@ -1,24 +1,24 @@
 export const Kick = (audioContext) => {
 	const output = audioContext.createGain()
+	output.gain.value = 0.5
 	const gains = [
 		audioContext.createGain(),
 		audioContext.createGain(),
 	]
 	const filter = audioContext.createBiquadFilter()
 	filter.type = 'lowshelf'
-	filter.frequency.value = 50
-	filter.gain.value = -1
+	filter.frequency.value = 200
+	filter.gain.value = -0.9
 	gains.forEach((gain) => {
 		gain.connect(filter)
-		gain.gain.setValueAtTime(1E-10, 0)
 	})
 	filter.connect(output)
 	let oscs = []
-	let subOscEnabled = true
+	let subOscEnabled = false
 	let mainOsc
 	let subOsc
 	let freq = 100
-	let finalFreq = 0.01
+	let finalFreq = 1
 	let duration = 0.25
 	let attack = 0.01
 
@@ -33,19 +33,19 @@ export const Kick = (audioContext) => {
 			]
 			if (subOscEnabled) {
 				subOsc = audioContext.createOscillator()
-				subOsc.type = 'triangle'
+				subOsc.type = 'sine'
 				subOsc.frequency.setValueAtTime(freq / 2, time)
 				subOsc.frequency.exponentialRampToValueAtTime(finalFreq / 2, time + duration)
 				oscs.push(subOsc)
 			}
-			gains.forEach((gain) => {
-				gain.gain.linearRampToValueAtTime((1 / oscs.length) * velocity, time + attack)
-				gain.gain.exponentialRampToValueAtTime(1E-10, time + duration)
-			})
 			oscs.forEach((osc, i) => {
 				osc.connect(gains[i])
 				osc.start(time)
 				osc.stop(time + duration)
+			})
+			gains.forEach((gain) => {
+				gain.gain.linearRampToValueAtTime((1 / gains.length) * velocity, time + attack)
+				gain.gain.exponentialRampToValueAtTime(0.001, time + duration)
 			})
 		},
 		noteOff(time = audioContext.currentTime + duration) {
