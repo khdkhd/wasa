e5ac94353f6ea825630efac095da9b062cfb7e49
Antoine Cordier <com.acordier@gmail.com>
2017-11-26 19:45:59 +0100

    Frequency from midi curry draft  (#18)
    * Frequency from midi curry draft
    
    * Fix lint

--- src/core/note.js
+++ src/core/note.js
@@ -1,3 +1,9 @@
+import { isNil } from 'ramda'
+
+/**
+ * Notes Durations Constants
+ * @type {Object}
+ */
 export const DURATIONS = Object.freeze({
 	WHOLE: 1,
 	HALF: 1 / 2,
@@ -5,24 +11,12 @@ export const DURATIONS = Object.freeze({
 	EIGHTH: 1 / 8,
 })
 
-export const Note = ({ note, octave, duration }) => ({
-	getNote() {
-		return note
-	},
-	getOctave() {
-		return octave
-	},
-	getDuration() {
-		return DURATIONS[duration] || duration
-	},
-})
-
 /**
  * Computes the frequency value of the given note in the given octave
  * @param {string} note - Note in scale (english notation)
  * @param {number} octave - Octave value for note
  */
-export const getFrequency = (note, octave) => {
+export const frequencyFromSymbol = (note, octave) => {
 	const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 	const o = 4 // base octave
 	const n = 'A' // base note
@@ -31,4 +25,23 @@ export const getFrequency = (note, octave) => {
 	const a = 2 ** (1 / 12) // semi tone factor
 	const m = octave - o >= 0 ? ((octave - o) + 1) : 1 / ((o - octave) + 1) // multiplier
 	return f * (a ** d) * m
+}
+
+/**
+ * Computes the frequency value of the given midi note
+ * with custom, optional tuning (default value for
+ * tuning is 440 for A4)
+ * This curry function will be partially applied if tuning
+ * is the only parameter
+ * @param {number} tuning - The frequency associated to midi value 69 (A4)
+ * @param {number} midiValue - Midi value (0 to 127) of the note
+ */
+export const frequencyFromMidi = (tuning = 440, midiValue) => {
+	if (isNil(midiValue)) {
+		return _ => frequencyFromMidi(tuning, _)
+	}
+	if (midiValue >= 0 && midiValue <= 127) {
+		return tuning * (2 ** ((midiValue - 69) / 12))
+	}
+	return null
 }
--- src/core/note.spec.js
+++ src/core/note.spec.js
@@ -1,63 +1,39 @@
 import test from 'ava'
-import { Note, getFrequency, DURATIONS } from '.'
+import { frequencyFromSymbol, frequencyFromMidi } from '.'
 
-test('Note factory creates an object', (t) => {
-	const note = Note({
-		note: 'A4',
-		octave: 3,
-		duration: DURATIONS.WHOLE,
-	})
-	t.true(typeof note === 'object')
-})
 
-test('Note factory creates an object with a note getter', (t) => {
-	const note = Note({
-		note: 'A4',
-		octave: 3,
-		duration: DURATIONS.WHOLE,
-	})
-	t.is('A4', note.getNote())
-})
-
-test('Note factory creates an object with an octave getter', (t) => {
-	const note = Note({
-		note: 'A4',
-		octave: 3,
-		duration: DURATIONS.WHOLE,
-	})
-	t.is(3, note.getOctave())
-})
-
-test('Note factory creates an object with a duration getter', (t) => {
-	const note = Note({
-		note: 'A4',
-		octave: 3,
-		duration: DURATIONS.WHOLE,
-	})
-	t.is(DURATIONS.WHOLE, note.getDuration())
-})
 test('Note C3 converts to 130.81 Hz', (t) => {
-	const frequency = getFrequency('C', 3)
+	const frequency = frequencyFromSymbol('C', 3)
 	t.is(130.81, Number(frequency.toFixed(2)))
 })
 
 test('Note A3 converts to 220 Hz', (t) => {
-	const frequency = getFrequency('A', 3)
+	const frequency = frequencyFromSymbol('A', 3)
 	t.is(220, frequency)
 })
 
 test('Note A#3 converts to 233.08 Hz', (t) => {
-	const frequency = getFrequency('A#', 3)
+	const frequency = frequencyFromSymbol('A#', 3)
 	t.is(233.08, Number(frequency.toFixed(2)))
 })
 
 test('Note A4 converts to 440 Hz', (t) => {
-	const frequency = getFrequency('A', 4)
+	const frequency = frequencyFromSymbol('A', 4)
 	t.is(440, frequency)
 })
 
 test('Note A5 converts to 880 Hz', (t) => {
-	const frequency = getFrequency('A', 5)
+	const frequency = frequencyFromSymbol('A', 5)
 	t.is(880, frequency)
 })
 
+test('Midi value 69 converts to 400 Hz with a 440 tuning value', (t) => {
+	const frequency = frequencyFromMidi(440, 69)
+	t.is(440, frequency)
+})
+
+test('Midi value 69 converts to 400 Hz with a 440 tuning value (curry)', (t) => {
+	const frequencyFromMidiPartial = frequencyFromMidi(/* defaults to 440 */)
+	const frequency = frequencyFromMidiPartial(69)
+	t.is(440, frequency)
+})
